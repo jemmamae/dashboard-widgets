d524a88b335093121aff1847ba24f2ec393acaa9
jemmamae <jmarshall13@ewu.edu>
2022-04-27 14:51:40 -0700

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -25,6 +25,7 @@ fetch(url)
     <data value="${data.name}" class="city">${data.name}</data>
     <data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data>
     <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Placeholder">
+    <data value="${data.wind.speed}" class="wind">${data.wind.speed}</data>
   `;
   
   // Insert dynamic template to container
